0758429b18a5dab7a16e3262cd3329535bd206a6
St Michael Egenamba <michaelegenamba@gmail.com>
2026-06-19 16:05:56 -0700

    Models is finished, added relationships, entites, and validators, as well as constraints

--- server/models.py
+++ server/models.py
@@ -16,6 +16,19 @@ class Exercise(db.Model):
 
     workout_exercises = db.relationship('WorkoutExercises', back_populates = 'exercise', cascade="all, delete-orphan")
 
+    # Exercise validators
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value or value.strip() == "":
+            raise ValueError("Name cannot be empty")
+        return value
+
+    @validates('category')        
+    def validate_category(self, key, value):
+        if not value or value.strip() == "":
+            raise ValueError("Category cannot be empty")
+        return value
+
 
 class Workout(db.Model):
     __tablename__ = 'workouts'
@@ -26,6 +39,21 @@ class Workout(db.Model):
     notes = db.Column(db.Text)
 
     workout_exercises = db.relationship('WorkoutExercises', back_populates = 'workout', cascade="all, delete-orphan")
+
+    # Workout validators
+    @validates('duration_minutes')
+    def validate_duration_minutes(self, key, value):
+        if value < 0:
+            raise ValueError("duration_minutes must be greater than 0")
+        return value
+    
+    @validates('date')
+    def validate_date(self, key, value):
+        if not value:
+            raise ValueError("Date cannot be empty")
+        if not isinstance(value, datetime):
+            raise ValueError("Invalid date format")
+        return value
 
 # Join Table
 class WorkoutExercises (db.Model):
@@ -41,4 +69,23 @@ class WorkoutExercises (db.Model):
     workout = db.relationship("Workout", back_populates="workout_exercises")
     exercise = db.relationship("Exercise", back_populates="workout_exercises")
 
+    # WorkoutExercises validators
+
+    @validates('reps')
+    def validate_reps(self, key, value):
+        if value < 0:
+            raise ValueError("Must be greater than 0")
+        return value
     
+    @validates('sets')
+    def validate_sets(self, key, value):
+        if value < 0:
+            raise ValueError("Must be greater than 0")
+        return value
+    
+    __table_args__ = (
+    CheckConstraint('reps > 0', name='check_reps_positive'),
+    CheckConstraint('sets > 0', name='check_sets_positive'),
+)
+
+
